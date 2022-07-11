62e5419501d97c77e9570ce2a52d642cd0951b01
isaiah20412 <73406518+isaiah20412@users.noreply.github.com>
2022-07-10 19:42:38 -0700

    Added the Analog Analyze script

---
+++ analog_analyze/analog_analyze.ino
@@ -0,0 +1,13 @@
+int LDRpin = 1;
+int LDRval = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  LDRval = analogRead(LDRpin);
+  Serial.println(LDRval);
+}
